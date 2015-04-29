27485457c67f8446e568970c2c6edaf114e4c273
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2015-04-30 02:00:08 +0900

    Use bootstrap, sass, reloader and update sinatra to 1.3.x

--- Gemfile
+++ Gemfile
@@ -1,6 +1,10 @@
 source "https://rubygems.org"
 ruby "2.2.0"
 
-gem 'sinatra', '1.1.0'
+gem 'sinatra', '~> 1.3.0'
+gem "bootstrap-sass"
+gem "compass"
+gem "sinatra-contrib"
+
 gem 'foreman'
 gem 'heroku'
--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
-require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/reloader' # on development
 
-get '/' do
-  "Hello, world"
+class App < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    "Hello, world"
+  end
+
 end
+
+App.run!
